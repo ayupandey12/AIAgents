2922b434e89f201627850cbdb90a2b79aad21793
ayupandey12 <ayushpandey9802@gmail.com>
2026-07-30 10:28:54 +0530

    all day1 done

--- week1/day1/index.py
+++ week1/day1/index.py
@@ -3,3 +3,19 @@ from groq import Groq
 from pathlib import Path
 from dotenv import load_dotenv
 
+load_dotenv()
+myapi_key=os.getenv("GROQ_API_KEY")
+if not myapi_key:
+    raise ValueError("key is not find")
+
+client=Groq(api_key=myapi_key)
+model="llama-3.3-70b-versatile"
+message={
+    "role":"user",
+    "content":"who is ayush pandey"
+}
+response=client.chat.completions.create(
+    messages=[message],
+    model=model
+)
+print(response.choices[0].message.content)
