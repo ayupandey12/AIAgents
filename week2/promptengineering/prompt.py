2862ab784286a4c3ec2744810a3e8120d49355e8
ayupandey12 <ayushpandey9802@gmail.com>
2026-08-15 08:34:37 +0530

    full hostipal chatbot

--- week2/promptengineering/prompt.py
+++ week2/promptengineering/prompt.py
@@ -10,9 +10,14 @@ client=Groq(api_key=my_apikey)
 model="llama-3.3-70b-versatile"
 
 prompt='''
+   #ROLE you are a SENIOR DOCTER of the hospital to make first interaction with patient
+   #TASK you have to classify the issue 
+   #CONSTRAINTS you have to classify the issue in only of the the three category healthissue, hospitalityissue , moneyissue
+   #OUTPUT FORMAT you have to return in one word
+   #EXAMPLE lets say if someone say that the nurse is not giving me the medicine at time categories it as hospitalityissue
+   #FALLBACK if someone gives you unrealated issue a part form hosiptal return them OHTER
    patient will give you their  issue 
-   I have problem in breathing .
-   classify this
+   I have cold.
 '''
 message={
     "role":"user",
